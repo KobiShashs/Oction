87a0850316e316d71487809605e33647050fe777
Kobi Shasha <kobi.shasha@gmail.com>
2017-07-02 03:05:45 +0300

    v0.0.1

--- app/src/main/java/btinnolab/com/oction/Fragments/AuctionCurrentFragment.java
+++ app/src/main/java/btinnolab/com/oction/Fragments/AuctionCurrentFragment.java
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -36,7 +37,7 @@ import btinnolab.com.oction.Utils.Utils;
  */
 public class AuctionCurrentFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Auction>>{
 
-
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private static final int AUCTION_LOADER_ID = 1;
     private AuctionAdapter mAdapter;
     private TextView mEmptyStateTextView;
@@ -82,6 +83,7 @@ public class AuctionCurrentFragment extends Fragment implements LoaderManager.Lo
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        runQuery();
     }
 
     @Override
@@ -89,6 +91,13 @@ public class AuctionCurrentFragment extends Fragment implements LoaderManager.Lo
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_auction_current, container, false);
+        // Retrieve the SwipeRefreshLayout and ListView instances
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_container);
+        // Set the color scheme of the SwipeRefreshLayout by providing 4 color resource ids
+        mSwipeRefreshLayout.setColorSchemeResources(
+                R.color.colorGreen, R.color.colorRed,
+                R.color.colorAccent, R.color.colorGrayDark);
+
         ListView auctionListView = (ListView) rootView.findViewById(R.id.list);
 
         mEmptyStateTextView = (TextView) rootView.findViewById(R.id.empty_view);
@@ -107,34 +116,22 @@ public class AuctionCurrentFragment extends Fragment implements LoaderManager.Lo
             }
         });
 
+        runQuery();
 
-        // Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        // Get details on the currently active default data network
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        // If there is a network connection, fetch data
-        if (networkInfo != null && networkInfo.isConnected()) {
-            // Get a reference to the LoaderManager, in order to interact with loaders.
-            LoaderManager loaderManager = getLoaderManager();
-
-            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-            // because this activity implements the LoaderCallbacks interface).
-            loaderManager.initLoader(AUCTION_LOADER_ID, null, this);
-        } else {
-            // Otherwise, display error
-            // First, hide loading indicator so error message will be visible
-            View loadingIndicator = rootView.findViewById(R.id.loading_indicator);
-            loadingIndicator.setVisibility(View.GONE);
-
-            // Update empty state with no connection error message
-            mEmptyStateTextView.setText(R.string.no_internet_connection);
-
-        }
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+               Utils.print("onRefresh called from SwipeRefreshLayout");
+
+                runQuery();
+            }
+        });
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -176,6 +173,34 @@ public class AuctionCurrentFragment extends Fragment implements LoaderManager.Lo
         void onFragmentInteraction(Uri uri);
     }
 
+    public void runQuery(){
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(AUCTION_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = rootView.findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+
+        }
+    }
     @Override
     public Loader<List<Auction>> onCreateLoader(int id, Bundle args) {
         return new AuctionLoader(getContext(), Utils.REQUEST_URL,true);
